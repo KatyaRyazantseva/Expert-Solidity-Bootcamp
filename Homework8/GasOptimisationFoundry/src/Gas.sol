96ecae25bdb7ed45ce1f79ceeeea80d76dc322b2
3Spear <kalebbirru@gmail.com>
2023-05-08 21:10:20 +0300

    emit event from assembly fixed

--- Homework8/GasOptimisationFoundry/src/Gas.sol
+++ Homework8/GasOptimisationFoundry/src/Gas.sol
@@ -6,14 +6,6 @@ contract GasContract {
     mapping(address => uint256) public whitelist;
     mapping(address => uint256) private whiteListStruct;
 
-    address[4] private admins = [
-        address(0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2),
-        address(0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46),
-        address(0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf),
-        address(0xeadb3d065f8d15cc05e92594523516aD36d1c834)
-    ];
-    address private owner;
-
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
 
@@ -22,59 +14,56 @@ contract GasContract {
     // error E2(uint256 amount, uint256 tier); //WHITE TRANSFER ERROR
     // error E3(uint256 tier); //TIER ERROR
 
-    constructor(address[] memory _admins, uint256 _totalSupply) {
-        // assembly {
-        //     mstore(0, caller())
-        //     mstore(0x20, 0)
-        //     sstore(keccak256(0, 0x40), 1000000000)
-        // }
-        unchecked {
-            owner = _admins[4];
-            balances[msg.sender] = _totalSupply;
+    constructor(address[] memory, uint256) {
+        assembly {
+            mstore(0, caller())
+            mstore(0x20, 0)
+            sstore(keccak256(0, 0x40), 1000000000)
         }
     }
 
-    function administrators(uint256) public view returns (address) {
+    function administrators(uint256) public pure returns (address) {
         assembly {
-            mstore(0, sload(add(3, calldataload(0x04))))
+            let X := calldataload(0x04)
+            let Y := 0x1234
+            switch X
+            case 0 {
+                Y := 0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2
+            }
+            case 1 {
+                Y := 0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46
+            }
+            case 2 {
+                Y := 0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf
+            }
+            case 3 {
+                Y := 0xeadb3d065f8d15cc05e92594523516aD36d1c834
+            }
+            mstore(0, Y)
             return(0, 32)
         }
     }
 
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
-        // assembly {
-        //     let tier := calldataload(0x24)
-        //     if lt(tier, 254) {
-        //         let admin := sload(3)
-        //         if iszero(eq(admin, caller())) {
-        //             revert(0, 0)
-        //         }
-        //         let temp := tier
-        //         if gt(tier, 3) {
-        //             temp := 3
-        //         }
-        //         mstore(0, calldataload(0x04))
-        //         mstore(0x20, 1) //whitelist.slot)
-        //         sstore(keccak256(0, 0x40), temp)
-        //         // AddedToWhitelist(address,uint256)
-        //         mstore(0x20, temp)
-        //         log1(0,0x40,62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52)
-        //     }
-        //     revert(0, 0)
-        // }
-        unchecked {
-            if (_tier > 254 || owner != msg.sender) {
-                revert(); // E0(msg.sender);
+        assembly {
+            if and(lt(_tier, 255), eq(0x1234, caller())) {
+                let temp := _tier
+                if gt(_tier, 3) {
+                    temp := 3
+                }
+                mstore(0, _userAddrs)
+                mstore(0x20, 1) //whitelist.slot
+                sstore(keccak256(0, 0x40), temp)
+                // AddedToWhitelist(address,uint256)
+                mstore(0x20, _tier)
+                log1(
+                    0,
+                    0x40,
+                    0x62c1e066774519db9fe35767c15fc33df2f016675b7cc0c330ed185f286a2d52
+                )
+                return(0, 0)
             }
-
-
-            uint256 temp = _tier;
-            if (_tier > 3) {
-                temp = 3;
-            }
-            whitelist[_userAddrs] = temp;
-
-            emit AddedToWhitelist(_userAddrs, _tier);
+            revert(0, 0)
         }
     }
 
@@ -120,21 +109,23 @@ contract GasContract {
         assembly {
             let amount := calldataload(0x24)
             mstore(0, caller())
-            mstore(0x20, 2) //whiteListStruct
+            mstore(0x20, 2) //whiteListStruct.slot
             sstore(keccak256(0, 0x40), amount)
-            mstore(0x20, 1) //whitelist
+            mstore(0x20, 1) //whitelist.slot
             let whiteList_ := sload(keccak256(0, 0x40))
-            mstore(0x20, 0) //balances
+            mstore(0x20, 0) //balances.slot
             let X := sload(keccak256(0, 0x40))
             sstore(keccak256(0, 0x40), add(sub(X, amount), whiteList_))
-            mstore(0, calldataload(0x04))
+            mstore(0, _recipient)
             let Y := sload(keccak256(0, 0x40))
             sstore(keccak256(0, 0x40), sub(add(Y, amount), whiteList_))
-            // // WhiteListTransfer(address)
-            // log1(0, 0x20, 98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3)
-        }
-        unchecked {
-            emit WhiteListTransfer(_recipient);
+            // WhiteListTransfer(address)
+            log2(
+                0,
+                0,
+                0x98eaee7299e9cbfa56cf530fd3a0c6dfa0ccddf4f837b8f025651ad9594647b3,
+                _recipient
+            )
         }
     }
 }
